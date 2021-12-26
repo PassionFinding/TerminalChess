41deaffdc97be8fbf795ce0d3a93fa0eefe6abf2
PassionFinding <JustUserAM@gmail.com>
2021-12-25 17:30:29 -0800

    simplify

--- Piece.py
+++ Piece.py
@@ -1,12 +1,3 @@
-#What does a piece in Chess have?
-#1. A color
-#2. A set of moves
-#3. A notation symbol
-#4. A position
-#5. A starting position
-#6. A legal move checker
-#   a. To see if the King will be in check
-#   b. To see if the piece is even allowed on that square
 layout = [["X" for i in range(8)] for i in range(8)]
 from string import ascii_uppercase as wordbabies
 a_h = list(wordbabies[:8])
@@ -69,13 +60,4 @@ class Piece:
   #player input will be set to p_move
     layout[int(self.position[-1]) - 1][a_h.index(self.position[-2])] = "X"
     layout[int(p_move[-1]) - 1][a_h.index(p_move[-2])] = self.notation
-    self.position = str(p_move[-2]) + str(p_move[-1])
-
-
-
-
-            
-                
-            
-                
-                    
+    self.position = str(p_move[-2]) + str(p_move[-1])    
