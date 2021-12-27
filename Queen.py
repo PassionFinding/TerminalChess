bea765089680842f9491e872266c7af9b97c74be
PassionFinding <JustUserAM@gmail.com>
2021-12-27 09:44:15 -0800

    Add queen

---
+++ Queen.py
@@ -0,0 +1,53 @@
+from Piece import Piece
+class Queen(Piece):
+  def __init__(self, color, notation, position, moved):
+    super().__init__(color, notation, position, moved)
+
+  def get_column(self):
+    column = []
+    for item in Piece.layout:
+      if  int(self.position[-1]) - 1 == Piece.layout.index(item):
+        for i in item:
+          if item.index(i) == Piece.a_h.index(self.position[-2]): 
+            column.append(str(Piece.a_h[Piece.a_h.index(item.index(i))]) + str(Piece.layout.index(item)))
+  
+  def get_row(self):
+    row = []
+    for item in Piece.layout:
+      if  int(self.position[-1]) - 1 == Piece.layout.index(item):
+        for i in item:
+          row.append(str(Piece.a_h[Piece.a_h.index(item.index(i))]) + str(Piece.layout.index(item)))
+  
+  def get_positive(self):
+    positive_diagonal = []
+    column_pos = Piece.a_h.index(self.position[-2])
+    row_pos = int(self.position[-1]-1)
+    while column_pos < 8 and row_pos < 8:
+      column_pos += 1
+      row_pos+= 1
+      positive_diagonal.append(str(Piece.a_h[column_pos]) + str(row_pos))
+    while column_pos >= 0 and row_pos >= 0:
+      column_pos -= 1
+      row_pos -= 1
+      positive_diagonal.append(str(Piece.a_h[column_pos]) + str(row_pos))
+    return positive_diagonal
+
+  def get_negative(self):
+    negative_diagonal = []
+    column_pos = Piece.a_h.index(self.position[-2])
+    row_pos = int(self.position[-1]-1)
+    while column_pos >= 0 and row_pos < 8:
+      column_pos -= 1
+      row_pos += 1
+      negative_diagonal.append(str(Piece.a_h[column_pos]) + str(row_pos))
+    while column_pos < 8 and row_pos >= 0:
+      column_pos += 1
+      row_pos -= 1
+      negative_diagonal.append(str(Piece.a_h[column_pos]) + str(row_pos))
+    return negative_diagonal
+  
+  def qlegal(self, move):
+      if self.legal(self.get_column(), move) == True or self.legal(self.get_row(), move) == True or self.legal(self.get_positive(), move) == True or self.legal(self.get_negative(), move) == True:
+          self.piece_move(move)
+      else:
+          print("Bro you can't do that")
