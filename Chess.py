fe93a288f261c92184273d29c780fab1c0fe3b20
PassionFinding <JustUserAM@gmail.com>
2021-12-28 21:24:36 -0800

    Edit the look of the piece notations and continue game functionality

--- Chess.py
+++ Chess.py
@@ -7,39 +7,126 @@ from Rook import Rook
 from Bishop import Bishop
 from Knight import Knight
 
-layout = [["X" for i in range(8)] for i in range(8)]
+layout = [["☐" for i in range(8)] for i in range(8)]
 a_h = list(wordbabies[:8])
-white_notation = ["R", "B", "N", "Q", "K", "P"]
-black_notation = ["r", "b", "n", "q", "k", "p"]
+white_notation = ["🆁", "🅱", "🅽", "🆀", "🅺", "🅿"]
+black_notation = ["🅁", "🄱", "🄽", "🅀", "🄺", "🄿"]
+upper_notation = ["P", "R", "B", "N", "Q", "K"]
 
 #Make a list of each sides pieces. When taken, list.pop(piece)
-white_a_pawn = Pawn(True, "P", "a2", False)
-white_b_pawn = Pawn(True, "P", "b2", False)
-white_c_pawn = Pawn(True, "P", "c2", False)
-white_d_pawn = Pawn(True, "P", "d2", False)
-white_e_pawn = Pawn(True, "P", "e2", False)
-white_f_pawn = Pawn(True, "P", "f2", False)
-white_g_pawn = Pawn(True, "P", "g2", False)
-white_h_pawn = Pawn(True, "P", "h2", False)
-white_left_rook = Rook(True, "R", "a1", False)
-white_right_rook = Rook(True, "R", "h1", False)
-white_left_knight = Knight(True, "N", "b1", False)
-white_right_knight = Knight(True, "N", "g2", False)
+white_a_pawn = Pawn(True, "🅿", "a2", False, False)
+white_b_pawn = Pawn(True, "🅿", "b2", False, False)
+white_c_pawn = Pawn(True, "🅿", "c2", False, False)
+white_d_pawn = Pawn(True, "🅿", "d2", False, False)
+white_e_pawn = Pawn(True, "🅿", "e2", False, False)
+white_f_pawn = Pawn(True, "🅿", "f2", False, False)
+white_g_pawn = Pawn(True, "🅿", "g2", False, False)
+white_h_pawn = Pawn(True, "🅿", "h2", False, False)
+white_left_rook = Rook(True, "🆁", "a1", False)
+white_right_rook = Rook(True, "🆁", "h1", False)
+white_left_knight = Knight(True, "🅽", "b1", False)
+white_right_knight = Knight(True, "🅽", "g1", False)
+white_left_bishop = Bishop(True, "🅱", "c1", False)
+white_right_bishop = Bishop(True, "🅱", "f1", False)
+white_queen = Queen(True, "🆀", "d1", False)
+white_king = King(True, "🅺", "e1", False)
+white_pieces = [white_a_pawn, white_b_pawn, white_c_pawn, white_d_pawn, white_e_pawn, white_f_pawn, white_g_pawn, white_h_pawn, white_left_rook,
+white_right_rook, white_left_knight, white_right_knight, white_left_bishop, white_right_bishop, white_queen, white_king]
 
+black_a_pawn = Pawn(False, "🄿", "a7", False, False)
+black_b_pawn = Pawn(False, "🄿", "b7", False, False)
+black_c_pawn = Pawn(False, "🄿", "c7", False, False)
+black_d_pawn = Pawn(False, "🄿", "d7", False, False)
+black_e_pawn = Pawn(False, "🄿", "e7", False, False)
+black_f_pawn = Pawn(False, "🄿", "f7", False, False)
+black_g_pawn = Pawn(False, "🄿", "g7", False, False)
+black_h_pawn = Pawn(False, "🄿", "h7", False, False)
+black_left_rook = Rook(False, "🅁", "h8", False)
+black_right_rook = Rook(False, "🅁", "a8", False)
+black_left_knight = Knight(False, "🄽", "g8", False)
+black_right_knight = Knight(False, "🄽", "b8", False)
+black_left_bishop = Bishop(False, "🄱", "f8", False)
+black_right_bishop = Bishop(False, "🄱", "c8", False)
+black_queen = Queen(False, "🅀", "d8", False)
+black_king = King(False, "🄺", "e8", False)
+black_piece = [black_a_pawn, black_b_pawn, black_c_pawn, black_d_pawn, black_e_pawn, black_f_pawn, black_g_pawn, black_h_pawn, black_right_rook, black_left_rook,
+black_right_knight, black_left_knight, black_right_bishop, black_left_bishop, black_queen, black_king]
 
 def printboard():
+    layout.reverse()
+    numb = list(range(1, 9))
+    numb.reverse()
+    num = iter(numb)
+    print("  A B C D E F G H")
     for thing in layout:
+        print(next(num), end = " ")
         print(" ".join(thing))
 
-def move():
-    player_move = input("Enter move. ")
+def reverseboard():
+    numb = list(range(1, 9))
+    num = iter(numb)
+    print("  🄰 🄱 🄲 🄳 🄴 🄵 🄶 🄷")
+    for thing in layout:
+        print(next(num), end = " ")
+        print(" ".join(thing))
 
 
-# while True:
-#     player_move = input("Enter move. ")
+move = True
+def black_move():
+    move = False
+    print("Black's move")
+def white_move():
+    move = True
+    print("White's move")
 
-#     player_move = list(player_move)
+def starting_board():
+    for item in black_piece:
+        layout[int(item.position[-1]) - 1][a_h.index(item.position[-2])] = item.notation
+    for item in white_pieces:
+        layout[int(item.position[-1]) - 1][a_h.index(item.position[-2])] = item.notation
 
-#     layout[int(player_move[1])-1][a_h.index(player_move[0])] = "p"
+def white_short_castle():
+    layout[0][4] = "☐"
+    layout[0][7] = "☐"
+    layout[0][5] = "R"
+    layout[0][6] = "K"
+def white_long_castle():
+    layout[0][4] = "☐"
+    layout[0][0] = "☐"
+    layout[0][3] = "R"
+    layout[0][2] = "K"
+def black_short_castle():
+    layout[7][4] = "☐"
+    layout[7][7] = "☐"
+    layout[7][5] = "R"
+    layout[7][6] = "K"
+def black_long_castle(): 
+    layout[7][4] = "☐"
+    layout[7][0] = "☐"
+    layout[7][3] = "R"
+    layout[7][2] = "K"
 
-#     printboard()
+p_move = input("Enter move: ")
+def move(p_move):
+    square = p_move[-2]
+    notation = p_move[0]
+    clarification = p_move[1]
+    if move == True:
+        if len(p_move) == 2:
+            pawns = []
+            for item in white_pieces:
+                if type(item) == Pawn:
+                    pawns.append(item)
+            for item in pawns:
+
+
+        black_move()
+
+while True:
+    starting_board()
+    printboard()
+    p_move = input("Enter move: ")
+    square = p_move[-2]
+    notation = p_move[0]
+    clarification = p_move[1]
+    
