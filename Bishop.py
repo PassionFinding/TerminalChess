824004be69428174c8c7de4f9c48a334b9d2f184
PassionFinding <92612812+PassionFinding@users.noreply.github.com>
2022-01-02 18:56:38 -0800

    Move all pieces under one file in order to make imports less complicated

--- Bishop.py
+++ Bishop.py
@@ -1,34 +1,35 @@
 from Piece import Piece
+from Piece import a_h
 class Bishop(Piece):
   def __init__(self, color, notation, position, moved):
     super().__init__(color, notation, position, moved)
   
   def get_positive(self):
     positive_diagonal = []
-    column_pos = Piece.a_h.index(self.position[-2])
+    column_pos = a_h.index(self.position[-2])
     row_pos = int(self.position[-1]-1)
     while column_pos < 8 and row_pos < 8:
       column_pos += 1
       row_pos+= 1
-      positive_diagonal.append(str(Piece.a_h[column_pos]) + str(row_pos))
+      positive_diagonal.append(str(a_h[column_pos]) + str(row_pos))
     while column_pos >= 0 and row_pos >= 0:
       column_pos -= 1
       row_pos -= 1
-      positive_diagonal.append(str(Piece.a_h[column_pos]) + str(row_pos))
+      positive_diagonal.append(str(a_h[column_pos]) + str(row_pos))
     return positive_diagonal
 
   def get_negative(self):
     negative_diagonal = []
-    column_pos = Piece.a_h.index(self.position[-2])
+    column_pos = a_h.index(self.position[-2])
     row_pos = int(self.position[-1]-1)
     while column_pos >= 0 and row_pos < 8:
       column_pos -= 1
       row_pos += 1
-      negative_diagonal.append(str(Piece.a_h[column_pos]) + str(row_pos))
+      negative_diagonal.append(str(a_h[column_pos]) + str(row_pos))
     while column_pos < 8 and row_pos >= 0:
       column_pos += 1
       row_pos -= 1
-      negative_diagonal.append(str(Piece.a_h[column_pos]) + str(row_pos))
+      negative_diagonal.append(str(a_h[column_pos]) + str(row_pos))
     return negative_diagonal
   
   def legal(self, move):
--- Rook.py
+++ Rook.py
@@ -3,29 +3,29 @@ class Rook(Piece):
   def __init__(self, color, notation, position, moved):
     super().__init__(color, notation, position, moved)
     
-  def get_column(self):
+  def get_column(self, board):
     column = []
-    for item in Piece.layout:
+    for item in board:
       if  int(self.position[-1]) - 1 == Piece.layout.index(item):
         for i in item:
           if item.index(i) == Piece.a_h.index(self.position[-2]): 
             column.append(str(Piece.a_h[Piece.a_h.index(item.index(i))]) + str(Piece.layout.index(item)))
   
-  def get_row(self):
+  def get_row(self, board):
     row = []
-    for item in Piece.layout:
+    for item in board:
       if  int(self.position[-1]) - 1 == Piece.layout.index(item):
         for i in item:
           row.append(str(Piece.a_h[Piece.a_h.index(item.index(i))]) + str(Piece.layout.index(item)))
 
-  def legal(self, move):
-    if self.llegal(self.get_column(), move) == True or self.llegal(self.get_row(), move) == True:
+  def legal(self, board, move):
+    if self.llegal(self.get_column(), board, move) == True or self.llegal(self.get_row(), board, move) == True:
       return True
     else:
       return False
   
-  def rlegal(self, move):
-    if self.llegal(self.get_column(), move) == True or self.llegal(self.get_row(), move) == True:
+  def rlegal(self, board, move):
+    if self.llegal(self.get_column(), board, move) == True or self.llegal(self.get_row(), board, move) == True:
       self.piece_move(move)
     else:
       print("Bro you can't do that")
